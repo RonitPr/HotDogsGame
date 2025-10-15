7c2c47c33a076b9dcd52b5205ba7563904b07a4d
Auri304 <tslilp304@gmail.com>
2025-10-15 04:06:10 +0300

    Added sprites etc

--- Assets/Scripts/TrapScripts/TrapSpawner.cs
+++ Assets/Scripts/TrapScripts/TrapSpawner.cs
@@ -12,7 +12,7 @@ public class TrapSpawner : MonoBehaviour
     //public List<GameObject> trapPrefabs; // assign 2 trap prefabs
     
     [Header("Trap Prefabs")]
-    public GameObject[] trapPrefabs; // Assign your trap prefabs here (Slime, Spikes, etc.)
+    public TrapController[] trapPrefabs; // Assign your trap prefabs here (Slime, Spikes, etc.)
 
     [Header("Spawn Settings")]
     [Range(0f, 1f)]
@@ -30,22 +30,11 @@ public class TrapSpawner : MonoBehaviour
     {
         foreach (TrapSpawnPoint point in spawnPoints)
         {
-            //float roll = Random.value;
-            //if (roll > spawnChance)
-            //{
-            //    Debug.Log($"No trap spawned at {point.name} (rolled {roll})");
-            //    continue;
-            //}
             if (Random.value > spawnChance) continue;
             
-            //GameObject randomTrap = trapPrefabs[Random.Range(0, trapPrefabs.Length)];
-            //Instantiate(randomTrap, point.transform.position, Quaternion.identity);
-            GameObject trapPrefab = trapPrefabs[Random.Range(0, trapPrefabs.Length)];
-            GameObject trapGO = Instantiate(trapPrefab, point.transform.position, Quaternion.identity);
+            TrapController trapPrefab = trapPrefabs[Random.Range(0, trapPrefabs.Length)];
+            TrapController trapController = Instantiate(trapPrefab, point.transform.position, Quaternion.identity);
 
-            //Debug.Log($"Spawned trap {randomTrap.name} at {point.name}");
-            
-            TrapController trapController = trapGO.GetComponent<TrapController>();
             if (trapController != null)
             {
                 trapController.SetDirection(point.direction);
