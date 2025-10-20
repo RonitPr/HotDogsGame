f393c8ef52f4b46e6e09158e73afef33beaeadd3
zotcode <ztavori@gmail.com>
2025-10-20 22:16:59 +0300

    Added sound to game
    Added sound will be played when game started
    Added tutorial
    Remove audio from intro screen

---
+++ Assets/Scripts/IntroScreen.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Introscreen : MonoBehaviour
+{
+    public string newGameScene;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(newGameScene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
