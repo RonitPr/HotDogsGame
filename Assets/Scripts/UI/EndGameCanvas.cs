14eb86777373bb26e25490ea5b3699a85d4cf22b
RonitPriav <42570726+RonitPr@users.noreply.github.com>
2025-10-27 21:17:10 +0200

    added "you won" text to end scene

--- Assets/Scripts/UI/EndGameCanvas.cs
+++ Assets/Scripts/UI/EndGameCanvas.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class EndGameCanvas : MonoBehaviour
 {
     public string introScene;
     public GameObject backButton;
+    public GameObject text;
     public int timeToWaitforButton = 10;
 
     public void GoToIntroScene()
@@ -14,12 +16,14 @@ public class EndGameCanvas : MonoBehaviour
     }
     void Start()
     {
-        StartCoroutine(ShowButton());
+        StartCoroutine(ShowCanvasElements());
     }
-    IEnumerator ShowButton()
+    IEnumerator ShowCanvasElements()
     {
         backButton.SetActive(false);
+        text.SetActive(false);
         yield return new WaitForSeconds(timeToWaitforButton);
         backButton.SetActive(true);
+        text.SetActive(true);
     }
 }
